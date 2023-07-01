fb0fa592bb5b8473a23f078097d5f66d2db4ccdc
emma wambui <emma.wambui@student.moringaschool.com>
2023-07-01 16:47:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function isPalindrome(word) {
+ function isPalindrome(str) {
+  const reversedStr = str.split('').reverse().join('');
+  return (str === reversedStr? "true": "false")
+}
+console.log(car);
+   
   // Write your algorithm here
 }
 
@@ -8,8 +14,6 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
